38175ac79680e5a06b244833bc97be7494202978
Felipe Carvalho <felipe_pc7@hotmail.com>
2023-08-11 21:12:20 -0300

    update: removed default todo list

--- src/context/TodoContext.jsx
+++ src/context/TodoContext.jsx
@@ -3,23 +3,7 @@ import { createContext, useState } from "react";
 export const TodoContext = createContext();
 
 export const TodoProvider = ({ children }) => {
-    const [todos, setTodos] = useState([
-        {
-            id: 1,
-            name: "Estudar",
-            completed: false,
-        },
-        {
-            id: 2,
-            name: "Trabalhar",
-            completed: false,
-        },
-        {
-            id: 3,
-            name: "Jogar bola",
-            completed: true,
-        },
-    ]);
+    const [todos, setTodos] = useState([]);
 
     const createTodo = todo => {
         const newTodo = [...todos, todo];
